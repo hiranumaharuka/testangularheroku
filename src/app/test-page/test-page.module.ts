98a3c36ee920ad950e9d37f5ca9fcebf5c2422e5
hiranumaharuka <colorcord251@gmail.com>
2020-07-08 00:23:37 +0900

    testページ内容追加

--- src/app/test-page/test-page.module.ts
+++ src/app/test-page/test-page.module.ts
@@ -3,8 +3,21 @@ import { CommonModule } from '@angular/common';
 import { TestPageRoutingModule } from './test-page-routing.module';
 import { TestPageComponent } from './test-page/test-page.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [TestPageComponent],
-  imports: [CommonModule, TestPageRoutingModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    TestPageRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
 })
 export class TestPageModule {}
--- src/app/test-page/test-page/test-page.component.ts
+++ src/app/test-page/test-page/test-page.component.ts
@@ -3,13 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test-page',
   templateUrl: './test-page.component.html',
-  styleUrls: ['./test-page.component.scss']
+  styleUrls: ['./test-page.component.scss'],
 })
 export class TestPageComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  // products$: Observable<Products[]> = this.crudService
+  //   .getProducts()
+  //   .pipe(map((data) => data.reverse()));
+  // products: Products[];
+  // dataSource = this.products$;
+  // sub = new Subscription();
+  // form = this.fb.group({
+  //   p_id: ['', [Validators.required, Validators.maxLength(10)]],
+  //   p_name: ['', [Validators.required, Validators.maxLength(40)]],
+  //   p_description: ['', [Validators.required, Validators.maxLength(100)]],
+  //   p_price: ['', [Validators.required, Validators.maxLength(40)]],
+  // });
+  // constructor(private crudService: CrudService, private fb: FormBuilder) {}
+  constructor() {}
 
   ngOnInit(): void {
+    // this.sub = this.products$.subscribe((data) => (this.products = data));
   }
 
+  // get idControl() {
+  //   return this.form.get('p_id') as FormControl;
+  // }
+  // get nameControl() {
+  //   return this.form.get('p_name') as FormControl;
+  // }
+  // get descriptionControl() {
+  //   return this.form.get('p_description') as FormControl;
+  // }
+  // get priceControl() {
+  //   return this.form.get('p_price') as FormControl;
+  // }
+
+  submit(): void {
+    // const formData = this.form.value;
+    // console.log(formData);
+    // this.crudService.createProduct(formData).subscribe((data) => {
+    //   console.log(data);
+    //   // this.products.push(data);
+    // });
+    console.log('登録');
+  }
 }
