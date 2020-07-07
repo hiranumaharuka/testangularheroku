2fe618ace4336aeff0b6cae301602f86ab23db52
hiranumaharuka <colorcord251@gmail.com>
2020-07-07 23:59:39 +0900

    angularmaterial使用

--- src/app/test-page/test-page.module.ts
+++ src/app/test-page/test-page.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TestPageRoutingModule } from './test-page-routing.module';
 import { TestPageComponent } from './test-page/test-page.component';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [TestPageComponent],
-  imports: [
-    CommonModule,
-    TestPageRoutingModule
-  ]
+  imports: [CommonModule, TestPageRoutingModule, MatButtonModule],
 })
-export class TestPageModule { }
+export class TestPageModule {}
