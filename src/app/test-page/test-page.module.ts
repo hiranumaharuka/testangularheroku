23e1a291d5003f7b96a0c23e27c67528bd061b79
hiranumaharuka <colorcord251@gmail.com>
2020-07-07 23:52:40 +0900

    Angularmaterialインストール

--- src/app/test-page/test-page.module.ts
+++ src/app/test-page/test-page.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { TestPageRoutingModule } from './test-page-routing.module';
 import { TestPageComponent } from './test-page/test-page.component';
-
-
 @NgModule({
   declarations: [TestPageComponent],
   imports: [
