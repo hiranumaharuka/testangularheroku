eb499d3ff9d5d962cbaf3839a5089d1de32d8866
hiranumaharuka <colorcord251@gmail.com>
2020-07-07 23:43:50 +0900

    テストページにscss追加

--- src/app/test-page/test-page-routing.module.ts
+++ src/app/test-page/test-page-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TestPageModule } from './test-page.module';
 import { TestPageComponent } from './test-page/test-page.component';
 
 
